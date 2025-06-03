15419f868e5ad4f888ae0734c40e12696c33534b
Erik Pohl <erik.pohl.444@gmail.com>
2025-06-03 11:52:03 -0400

    improved exception handling -- still needs a lot of work -- and verification output

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import json
+import logging
 
 import requests
 from datetime import datetime, timedelta
 from flask import Flask, Response, stream_with_context
 from flask_cors import CORS
 import time
+from collections import Counter
 
 from requests import HTTPError, TooManyRedirects
 
@@ -63,23 +65,33 @@ def obs_stream() -> Response:
         data: json = response.json()
     except ConnectionError:
         print("connection error")
+        exit(1)
     except HTTPError:
         print("http error")
+        exit(1)
     except TimeoutError:
         print("timeout error")
+        exit(1)
     except TooManyRedirects:
         print("too many redirects")
+        exit(1)
     try:
         print(f"obtained data for these places")
-        for result in data['results']:
-            print(result['place_guess'])
+        [
+            print(f"{place_name}: {count}")
+            for (place_name, count)
+            in Counter([item['place_guess']
+                        for item in data['results']]
+                       ).items()
+        ]
+        # filter the data by places and having a species guess
         place_terms = ['Arlington', 'Horn Pond', 'Concord']
         filtered_data: list[dict] = [
             result for result in data['results']
             if result['species_guess'] and contains_any_substring(result['place_guess'], place_terms)
         ]
-    except:
-        pass
+    except TypeError:
+        logging.error("issue outputting or filtering results ")
 
     # Output results
     print(f"Found {len(filtered_data)} observations in filtered places with a species guess in the last 24 hours:")
@@ -94,7 +106,7 @@ def obs_stream() -> Response:
                 "obs_place_guess": obs['place_guess'],
                 "obs_observed_on_string": obs['observed_on_string']
             }
-        except:
+        except TypeError:
             print("encountered an exception")
         result_obs.append(data)
     return Response(stream_with_context(generate_data(result_obs)), mimetype='text/event-stream')
